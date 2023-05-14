28de9983dc99514d4cda567146c9bac3643f01e4
DESKTOP-5TCD4T0\EQUIPO <f>
2023-05-15 00:13:09 +0200

    1.2

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -1,21 +1,46 @@
+var id="";
 function guardarDatos(){
-		
+
 let edad = document.getElementsByName('edad')[0].value;
 let nombre = document.getElementsByName('nombre')[0].value;
 let apellidos = document.getElementsByName('apellidos')[0].value;
+let dni = document.getElementsByName('dni')[0].value;
 
-localStorage.setItem("nombre", nombre);
-localStorage.setItem("apellidos", apellidos);
-localStorage.setItem("edad", edad);
-		
+if(localStorage.getItem("id") == null)
+	localStorage.setItem("id", 0);
+	
+id = parseInt(localStorage.getItem("id"));
+
+localStorage.setItem("id", id+1);
+
+localStorage.setItem(id+"nombre", nombre);
+localStorage.setItem(id+"apellidos", apellidos);
+localStorage.setItem(id+"edad", edad);
+localStorage.setItem(id+"dni", dni);
+
 window.open("mostrarDatos.html")
+
+for(let i=1;i<=id;i++)
+	{		
+		document.write("<br><br>Usuario numero "+i)
+		document.write("<br>Nombre: "+localStorage.getItem(i+"nombre"))
+		document.write("<br>Apellidos: "+localStorage.getItem(i+"apellidos"))
+		document.write("<br>Edad: "+localStorage.getItem(i+"edad"))
+		document.write("<br>DNI: "+localStorage.getItem(i+"dni"))		
+	}
 
 }
 
 function MostrarDatos(){
 
-document.write("Nombre: "+localStorage.getItem("nombre"))
-document.write("<br>Apellidos: "+localStorage.getItem("apellidos"))
-document.write("<br>Edad: "+localStorage.getItem("edad"))
-
+for(let i=1;i<=id;i++)
+	{		
+		document.write("<br><br>Usuario numero "+i)
+		document.write("<br>Nombre: "+localStorage.getItem(i+"nombre"))
+		document.write("<br>Apellidos: "+localStorage.getItem(i+"apellidos"))
+		document.write("<br>Edad: "+localStorage.getItem(i+"edad"))
+		document.write("<br>DNI: "+localStorage.getItem(i+"dni"))		
+	}
 }
+
+
