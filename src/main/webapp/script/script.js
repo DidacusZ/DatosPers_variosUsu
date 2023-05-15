4f5d488c8164577db91109eae96f599ed3e3c1d5
DidacusZ <diegoooooooooooooooo16@gmail.com>
2023-05-15 10:17:52 +0200

    fin

--- src/main/webapp/script/script.js
+++ src/main/webapp/script/script.js
@@ -1,46 +1,40 @@
-var id="";
-function guardarDatos(){
 
-let edad = document.getElementsByName('edad')[0].value;
-let nombre = document.getElementsByName('nombre')[0].value;
-let apellidos = document.getElementsByName('apellidos')[0].value;
-let dni = document.getElementsByName('dni')[0].value;
+class Usuario{
+	constructor (dni,edad,apellidos,nombre) {
+	    this.dni = dni;
+	    this.edad = edad;
+	    this.nombre = nombre;
+	    this.apellidos = apellidos;
+	    }
+}
 
-if(localStorage.getItem("id") == null)
-	localStorage.setItem("id", 0);
+function GuardarDatos(formulario){
+
+//se guardan los datos del formulario
+var dni=formulario.dni.value;
+var edad=formulario.edad.value;
+var nombre=formulario.nombre.value;
+var apellidos=formulario.apellidos.value;
+
+//creamos objeto usuario
+var usuario = new Usuario(dni,edad,apellidos,nombre);
+
+//guardamos objeto en memoria local/   con dni se guarda en memoria
+localStorage.setItem(dni, JSON.stringify(usuario));//JSON.stringify: convierte objeto a cadena JSON
+
+}
+
+function Consultar(formulario2){
 	
-id = parseInt(localStorage.getItem("id"));
+//guardamos el dni a consultar
+var dniConsultar = formulario2.dni.value;
 
-localStorage.setItem("id", id+1);
+//JSON.parse: convierte cadena JSON a objeto
+var obj = JSON.parse(localStorage.getItem(dniConsultar));
 
-localStorage.setItem(id+"nombre", nombre);
-localStorage.setItem(id+"apellidos", apellidos);
-localStorage.setItem(id+"edad", edad);
-localStorage.setItem(id+"dni", dni);
-
-window.open("mostrarDatos.html")
-
-for(let i=1;i<=id;i++)
-	{		
-		document.write("<br><br>Usuario numero "+i)
-		document.write("<br>Nombre: "+localStorage.getItem(i+"nombre"))
-		document.write("<br>Apellidos: "+localStorage.getItem(i+"apellidos"))
-		document.write("<br>Edad: "+localStorage.getItem(i+"edad"))
-		document.write("<br>DNI: "+localStorage.getItem(i+"dni"))		
-	}
-
+//se muestran datos del objeto elegido
+document.write("<br>Nombre: "+obj.nombre)
+document.write("<br>Apellidos: "+obj.apellidos)
+document.write("<br>Edad: "+obj.edad)
+document.write("<br>DNI: "+obj.dni)
 }
-
-function MostrarDatos(){
-
-for(let i=1;i<=id;i++)
-	{		
-		document.write("<br><br>Usuario numero "+i)
-		document.write("<br>Nombre: "+localStorage.getItem(i+"nombre"))
-		document.write("<br>Apellidos: "+localStorage.getItem(i+"apellidos"))
-		document.write("<br>Edad: "+localStorage.getItem(i+"edad"))
-		document.write("<br>DNI: "+localStorage.getItem(i+"dni"))		
-	}
-}
-
-
